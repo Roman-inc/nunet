dbf021e930d83b67b038691a6db498d68151fc38
Roman Onyshkiv <romaonyshkiv@gmail.com>
2025-01-15 00:34:57 -0500

    AND OR XOR

--- AND_operator.py
+++ AND_operator.py
@@ -3,7 +3,7 @@ import numpy as np
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 outputs = np.array([0, 0, 0, 1])
 weights = np.array([0.0, 0.0])
-learning_rate = 0.1
+learning_rate = 0.0001
 
 
 def step_function(sum):
@@ -19,8 +19,10 @@ def calculate_output(instance):
 
 def train():
     total_error = 1
+    iteration = 1
     while total_error != 0:
         total_error = 0
+        print(f'Iteration: {str(iteration)}')
         for i in range(len(outputs)):
             prediction = calculate_output(inputs[i])
             error = abs(outputs[i] - prediction)
@@ -29,6 +31,7 @@ def train():
                 for j in range(len(weights)):
                     weights[j] = weights[j] + (learning_rate * inputs[i][j] * error)
                     print(f'Weight updated: {str(weights[j])}')
+        iteration += 1
         print(f'Total errors: {str(total_error)}')
 
 
